7bd1c55803d37e0b3aba03032c24aed19992906f
CLASE <CLASE@LAPTOP-HK1DJLBJ>
2020-11-17 00:42:54 +0100

    desacoplado + videojs

---
+++ index.js
@@ -0,0 +1,79 @@
+var modoOculto = false;
+		var myVideo = document.getElementById("miVideo");
+		var cuentaReinicios = document.getElementById("cuentaReinicios");
+		var cuentaPausa = document.getElementById("cuentaPausa");
+		var estadoModo = document.getElementById("estadoModo");
+		var botones = document.getElementById("botones");
+		var fondo = document.getElementById("fondo");
+		var tituloOcultable = document.getElementById("tituloOcultable");
+		var estadisticasOcultable = document
+				.getElementById("estadisticasOcultable");
+		var contR = 1;
+		var contP = 1;
+
+		var video = videojs('miVideo', {
+
+			fluid : true,
+			loop : false,
+			controls : false
+
+		});
+
+		function play() {
+			if (myVideo.paused)
+				myVideo.play();
+		}
+		function pause() {
+			if (!myVideo.paused) {
+				myVideo.pause();
+				cuentaPausa.innerHTML = contP++;
+			}
+		}
+		function reiniciar() {
+			myVideo.load();
+			cuentaReinicios.innerHTML = contR++;
+			contP = 1;
+			cuentaPausa.innerHTML = 0;
+
+		}
+		function makeBig() {
+			myVideo.width = myVideo.width + 40;
+		}
+
+		function makeSmall() {
+			myVideo.width = myVideo.width - 40;
+		}
+
+		function makeNormal() {
+			myVideo.width = 900;
+		}
+
+		function acelerar() {
+			myVideo.playbackRate = 5.0;
+		}
+		function frenar() {
+			myVideo.playbackRate = 1.0;
+		}
+		
+		function ocultar() {
+			if (modoOculto == false) {
+				modoOculto = true;
+				estadoModo.innerHTML = 'ON';
+				estadisticasOcultable.style.display = 'none';
+				tituloOcultable.style.display = 'none';
+				myVideo.width = 1100;
+				myVideo.style = "margin-top: 3em"
+				fondo.style = "background-color : black";
+				botones.style= "margin-top: 3em";
+			} else {
+				modoOculto = false;
+				estadoModo.innerHTML = "OFF";
+				estadisticasOcultable.style.display = 'block';
+				tituloOcultable.style.display = 'block';
+				myVideo.width = 900;
+				myVideo.style = "margin-down: -3em"
+				fondo.style = "background-color : white";
+				botones.style= "margin-down: -3em";
+
+			}
+		}
